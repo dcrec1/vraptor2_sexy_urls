b35461f1fbe560fcc448f321d600d3c0aca36e50
Diego Carrion <dcrec1@dcrec1.(none)>
2008-12-11 10:57:35 -0200

    updated to version 2.6

--- src/test/java/org/vraptor/VRaptorFilterTest.java
+++ src/test/java/org/vraptor/VRaptorFilterTest.java
@@ -13,36 +13,45 @@ import org.vraptor.component.LogicNotFoundException;
 import org.vraptor.core.Controller;
 import org.vraptor.test.MockedServletContext;
 
-public class VRaptorFilterTest extends AbstractTest{
+public class VRaptorFilterTest extends AbstractTest {
 
 	private Mock controllerMock;
 	private Mock chain;
 	private VRaptorFilter filter;
 
-	public void testInvokesItsControllerExecution() throws ServletException, IOException {
+	public void testInvokesItsControllerExecution() throws ServletException,
+			IOException {
 
 		controllerMock.expects(once()).method("execute");
-		
+
 		filter.doFilter(null, null, (FilterChain) chain.proxy());
 
 	}
 
-	public void testGivesA404IfLogicNotFound() throws ServletException, IOException {
+	public void testGivesA404IfLogicNotFound() throws ServletException,
+			IOException {
 
-		controllerMock.expects(once()).method("execute").will(throwException(new LogicNotFoundException("")));
+		controllerMock.expects(once()).method("execute").will(
+				throwException(new LogicNotFoundException("")));
 		Mock response = mock(HttpServletResponse.class);
-		response.expects(once()).method("sendError").with(eq(404), isA(String.class));
-		filter.doFilter(null, (HttpServletResponse) response.proxy(), (FilterChain) chain.proxy());
+		response.expects(once()).method("sendError").with(eq(404),
+				isA(String.class));
+		filter.doFilter(null, (HttpServletResponse) response.proxy(),
+				(FilterChain) chain.proxy());
 
 	}
 
-	public void testGivesA404IfComponentNotFound() throws ServletException, IOException {
+	public void testGivesA404IfComponentNotFound() throws ServletException,
+			IOException {
 
-		controllerMock.expects(once()).method("execute").will(throwException(new ComponentNotFoundException("")));
+		controllerMock.expects(once()).method("execute").will(
+				throwException(new ComponentNotFoundException("")));
 		Mock response = mock(HttpServletResponse.class);
-		response.expects(once()).method("sendError").with(eq(404), isA(String.class));
-		
-		filter.doFilter(null, (HttpServletResponse) response.proxy(), (FilterChain) chain.proxy());
+		response.expects(once()).method("sendError").with(eq(404),
+				isA(String.class));
+
+		filter.doFilter(null, (HttpServletResponse) response.proxy(),
+				(FilterChain) chain.proxy());
 
 	}
 
@@ -52,7 +61,8 @@ public class VRaptorFilterTest extends AbstractTest{
 		this.controllerMock = mock(Controller.class);
 		this.filter = new VRaptorFilter((Controller) controllerMock.proxy());
 		Mock config = mock(FilterConfig.class);
-		config.expects(once()).method("getServletContext").will(returnValue(new MockedServletContext()));
+		config.expects(once()).method("getServletContext").will(
+				returnValue(new MockedServletContext()));
 		chain = mock(FilterChain.class);
 		filter.init((FilterConfig) config.proxy());
 	}
